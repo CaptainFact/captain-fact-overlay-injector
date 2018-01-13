c847e90db163ded2503a9325db3f73d7653110f9
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-01-14 01:54:25 +1100

    Fix tests

--- src/components/Fact/Fact.spec.js
+++ src/components/Fact/Fact.spec.js
@@ -1,18 +1,18 @@
-import Fact from './Fact'
+import { Fact } from './Fact'
 
 
 test("approving fact", () => {
-  snapshot(<Fact comment={{user: {id: 42}, approve: true, score: 1337}}/>)
+  snapshot(<Fact comment={{user: {id: 42}, approve: true, score: 1337}} graphics={{}}/>)
 })
 
 test("refuting fact", () => {
-  snapshot(<Fact comment={{user: {id: 42}, approve: false, score: 1337}}/>)
+  snapshot(<Fact comment={{user: {id: 42}, approve: false, score: 1337}} graphics={{}}/>)
 })
 
 test("with text", () => {
-  snapshot(<Fact comment={{user: {id: 42}, approve: true, score: 1337, text: "Foobar"}}/>)
+  snapshot(<Fact comment={{user: {id: 42}, approve: true, score: 1337, text: "Foobar"}} graphics={{}}/>)
 })
 
 test("custom star image", () => {
-  snapshot(<Fact comment={{user: {id: 42}, approve: true, score: 1337}} imgStar="custom.jpg"/>)
+  snapshot(<Fact comment={{user: {id: 42}, approve: true, score: 1337}} graphics={{star: "custom.jpg"}}/>)
 })
