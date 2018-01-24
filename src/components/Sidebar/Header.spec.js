707d644bb24d0f10758960202a1bb54d94969b64
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-01-24 13:08:12 +1100

    Fix tests

--- src/components/Sidebar/Header.spec.js
+++ src/components/Sidebar/Header.spec.js
@@ -2,20 +2,20 @@ import {Header} from './Header'
 
 
 test('default render', () => {
-  snapshot(<Header videoId={42}/>)
+  snapshot(<Header videoHashId={"xxxx"}/>)
 })
 
 test('show toggle if given a close function', () => {
-  snapshot(<Header videoId={50} onCloseClick={() => 42}/>)
+  snapshot(<Header videoHashId={"xxxx"} onCloseClick={() => 42}/>)
 })
 
 test('new tab image can be override', () => {
-  snapshot(<Header videoId={42} imgNewTab="overrided-pic.jpg"/>)
+  snapshot(<Header videoHashId={"xxxx"} imgNewTab="overrided-pic.jpg"/>)
 })
 
 test('call function when clicking on close', () => {
   const closeFunc = jest.fn()
-  const mounted = mount(<Header videoId={42} onCloseClick={closeFunc}/>)
+  const mounted = mount(<Header videoHashId={"xxxx"} onCloseClick={closeFunc}/>)
 
   mounted.find('.closeBtn').first().simulate('click')
   expect(closeFunc).toBeCalled()
